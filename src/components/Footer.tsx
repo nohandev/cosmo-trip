6c6fcf621182f111f19c266263c31044f8e09657
nohandev <nohanpablo759@gmail.com>
2026-01-01 15:08:39 -0300

    fix: form in footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,28 @@
-import Button from '@/components/ui/Button'
 import { ArrowRight } from 'lucide-react'
 
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const schema = z.object({
+  email: z
+    .string()
+    .min(1, {error: 'Preencha o campo vazio!'})
+    .email({error: 'Certifique-se de que o e-mail é válido!'})
+})
+
+type FormDataType = z.infer<typeof schema>
+
 const Footer = () => {
+
+  const { register, handleSubmit, formState: { errors } } = useForm<FormDataType>({
+		resolver: zodResolver(schema),
+	});
+
+  const onSubmit = (data: FormDataType) => {
+    window.alert(`Cadastro enviado para ${data.email}`)
+  }
+  
   return (
     <footer className='bg-black py-24' id='contato'>
       <article className='w-full max-w-8xl flex justify-between mx-auto'>
@@ -47,28 +68,38 @@ const Footer = () => {
             <h2 className='font-inter text-white text-xl'>Join our mailing list for updates</h2>
           </div>
 
-          <form className='mt-6'>
-            <div className='flex flex-col'>
-              <label htmlFor='email' className='text-gray font-alexandria text-sm'>
-                Your email address
-              </label>
-              <div className='relative'>
-                <input
-                 type='email'
-                 name='email'
-                 id='email'
-                 className='border-b border-white/50 w-full py-2 text-white focus:outline-none'/>
+          <div className='mt-6'>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className='w-full flex flex-col gap-1'>
+                <label 
+                  className='text-gray font-alexandria text-sm'
+                  htmlFor='email'>
+                  Your email address
+                </label>
 
-                <Button 
-                  variant='circle'
-                  className='absolute top-1/2 -translate-y-1/2 right-0'
-                  type='submit'
-                  onClick={(e: React.FormEvent<HTMLButtonElement>)=> e.preventDefault()}>
-                 <ArrowRight size={14}/>
-                </Button>
+                <div className='w-full relative'>
+                  <input 
+                    type='text'
+                    id='email'
+                    {...register('email')}
+                    className='bg-transparent w-full outline-none p-1 border-b border-white/50 text-white'
+                    />
+
+                    <button 
+                     className='absolute bg-pink text-white p-1 rounded-full right-1 top-1/2 -translate-y-1/2 cursor-pointer transition hover:scale-110'
+                     type='submit'
+                     >
+                      <ArrowRight size={16}/>
+                    </button>
+                </div>
+
+                <div className='text-center font-alexandria py-1 text-sm'>
+                  {errors && <span className='text-danger'>{errors.email?.message}</span>}
+                </div>
+
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </article>
     </footer>
